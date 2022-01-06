62c50e9f50d04c3d9561842d70f502ecbec326b9
Keido Sumi <delta0726@nta.pial.jp>
2022-01-07 06:31:39 +0900

    Update step3

--- practice/step03.py
+++ practice/step03.py
@@ -2,7 +2,6 @@
 # Title       : ゼロから作るDeep Learning3 （フレームワーク編）
 # Stage       : 1 微分を自動で求める
 # Step        : 03 関数の連結
-# Theme       : Exp関数の実装
 # Create Date : 2021/10/04
 # Update Date : 2022/01/06
 # Page        : P15 - P17
